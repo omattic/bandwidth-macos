dce5a8625bec7c1b7e719aa4c9fe50dd31f21c21
Carlos Guerrero <git@carlosguerrero.com>
2025-03-19 17:46:01 -0300

    Add quit button

--- main.swift
+++ main.swift
@@ -29,3 +29,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 // Create the application and delegate
 let app = NSApplication.shared
 let delegate = AppDelegate()
+app.delegate = delegate
+
+// Run the application
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
